87073b580b40f477195a2e5a450db3b2f32ef5b8
ParkGiraffe <op5321@soongsil.ac.kr>
2023-03-21 22:52:37 +0900

    책 내용 부분만 추출하기 구현 / 클립보드에 복사하기 구현

--- src/getDom.js
+++ src/getDom.js
@@ -1,9 +1,19 @@
-function getDom(document_body) {
-  console.log(document_body.innerText);
-  return document_body.innerText;
+// function getDom(document_body) {
+//   console.log(document_body.innerText);
+//   return document_body.innerText;
+// }
+
+function getDom(domTexts) {
+  const chunk = [];
+  domTexts.forEach(e => {
+    chunk.push(e.innerText);
+  })
+
+  return chunk;
 }
 
 chrome.runtime.sendMessage({
   action: "getSource",
-  source: getDom(document.querySelector('#page_reading_note_detail > article > div.css-18obxw2 > ul')),
+  // source: getDom(document.querySelector('#page_reading_note_detail > article > div.css-18obxw2 > ul')),
+  source: getDom(document.querySelectorAll('.css-135eg6y')),
 });
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,24 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Popup.css";
 
 function Popup() {
-  const buttonHandelr = (event: React.MouseEvent) => {
-    event.preventDefault();
+  const [texts, setTexts] = useState<string[]>([]);
+
+  const crawlButtonHandler = (event: React.MouseEvent) => {
+    // event.preventDefault();
     chrome.runtime.sendMessage({ action: "CHECK" });
-    console.log("execute");
 
     chrome.runtime.onMessage.addListener(function (request, sender) {
-      console.log(request.source);
       if (request.action == "getSource") {
-        console.log(request.source);
-        document.body.innerText = request.source;
+        setTexts(request.source);
       }
+      // 여기에서 클립보드를 복사하려고 하면, state의 비동기성 때문에 클립보드에 빈 칸만 복사가 된다.
     });
+  };
+
+  const copyButtonHandler = () => {
+    navigator.clipboard.writeText(texts.join("\n"));
   };
 
   return (
     <div className="Popup">
-      <button onClick={buttonHandelr}>crawl hilights in this page</button>
+      <button onClick={crawlButtonHandler}>crawl hilights</button>
+      <button onClick={copyButtonHandler}>copy hilights</button>
+      {texts.map((text) => (
+        <p>{text}</p>
+      ))}
     </div>
   );
 }
